99d2e66e913bb6e9c31b65a2191b5aea6a4d2ae6
Glen Paul Marquez <glenpaulmarquez01@gmail.com>
2024-11-26 21:37:57 +0800

    aral

---
+++ Other Files/enroll.js
@@ -0,0 +1,8 @@
+function handleSubmit(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Show the alert message
+    alert("ARAL NA");
+    window.location.href = "landing page.html";
+}
